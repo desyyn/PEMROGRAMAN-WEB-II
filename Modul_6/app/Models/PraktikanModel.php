9da1c7c447ff3db7489590e2af0ef029002b744e
Dessy <2310817220024.mhs.ulm.ac.id>
2025-05-16 11:05:22 +0800

    Add Modul_6

---
+++ Modul_6/app/Models/PraktikanModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PraktikanModel extends Model
+{
+    public function getData()
+    {
+        return [
+            'nama' => 'Dessy Nurulita',
+            'nim' => '2310817220024',
+            'asal_prodi' => 'Teknologi Informasi',
+            'hobi' => ['Baking', 'Listening to music', 'Playing Roblox'],
+            'skill' => ['design', 'editing'],
+            'comfort_food' => ['pisang keju', 'terang bulan', 'mie kuah'],
+            'comfort_song' => ['atlas - keshi', 'to let a good thing die - bruno major', 'photograph - ed sheeran']
+        ];
+    }
+}
