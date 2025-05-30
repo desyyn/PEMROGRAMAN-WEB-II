30397e20d5db9043cb7243eb9b147e7bb8cda370
Dessy Nurulita <144782365+desyyn@users.noreply.github.com>
2025-05-30 20:36:05 +0800

    Delete Modul_6 directory

--- Modul_6/app/Config/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Praktikan::index');
-$routes->get('/praktikan/profil', 'Praktikan::profil');
--- Modul_6/app/Views/beranda.php
+++
@@ -1,7 +0,0 @@
-<section class="center">
-    <h1>╰┈➤ Halo! Welcome to My Profile ^_^</h1>
-    <div class="card">
-        <p>Nama: <?= $nama ?></p>
-        <p>NIM: <?= $nim ?></p>
-    </div>
-</section>
